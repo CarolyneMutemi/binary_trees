f0b29f8449e456f02d9376b986c6c30ec1e7f45b
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-11-04 23:52:54 +0300

    102

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -18,22 +18,28 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (!check_leaf(tree->left) && !check_leaf(tree->right))
 		if (tree->left->left && !tree->left->right)
 			return (0);
-	if (tree->left && tree->right && check_leaf(tree->left) && check_leaf(tree->right))
+	if (tree->left && tree->right && check_leaf(tree->left)
+			&& check_leaf(tree->right))
 		return (1);
 	if (tree->left && !tree->right && check_leaf(tree->left))
 		return (1);
 
 
-	return binary_tree_is_complete(tree->left) && binary_tree_is_complete(tree->right);
+	return (binary_tree_is_complete(tree->left) &&
+			binary_tree_is_complete(tree->right));
 }
 
-
+/**
+ * check_leaf - checks if a tree has no children including an empty tree.
+ * @tree: pointer to the root node of the tree.
+ * Return: true if has no children, false if has children.
+ */
 
 bool check_leaf(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (false);
 	if (!tree->left && !tree->right)
-		return true;
-	return false;
+		return (true);
+	return (false);
 }
